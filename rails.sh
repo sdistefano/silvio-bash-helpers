9cb3998614a8485bfc1ddc63426ea6f1193b26f3
Silvio Di Stefano <sdistefano@gmail.com>
2019-06-27 10:42:08 -0400

    fixes

--- rails.sh
+++ rails.sh
@@ -2,17 +2,17 @@
 
 function alloc_dump {
     PID=$1
-    sudo rbtrace -p $PID -e 'Thread.new{GC.start;require "objspace";io=File.open("/tmp/ruby-heap.dump", "w"); ObjectSpace.dump_all(output: io); io.close}'
+    rbtrace -p $PID -e 'Thread.new{GC.start;require "objspace";io=File.open("/tmp/ruby-heap.dump", "w"); ObjectSpace.dump_all(output: io); io.close}'
 }
 
 function alloc_start {
     PID=$1
-    sudo rbtrace -p $PID -e 'Thread.new{GC.start;require "objspace";ObjectSpace.trace_allocations_start}'
+    rbtrace -p $PID -e 'Thread.new{GC.start;require "objspace";ObjectSpace.trace_allocations_start}'
 }
 
 function puma_restart {
     PID=$1
-    sudo kill -USR2 -p $PID
+    sudo kill -USR2 $PID
 }
 
 function puma_pid {
@@ -21,9 +21,21 @@ function puma_pid {
 
 function puma_reduce {
     PID=$1
-    sudo kill -TTOU -p $PID
+    sudo kill -TTOU $PID
 }
 
 function puma_worker_pid {
     ps ax|grep  puma| grep 'cluster worker' | head -1| awk '{ print $1 }'
+}
+
+function rbtools_install {
+    sudo gem install rbtrace heapy
+}
+
+function rbtrace {
+    sudo /usr/local/rvm/gems/ruby-2.?.?/wrappers/rbtrace "$@"
+}
+
+function heapy {
+    sudo /usr/local/rvm/gems/ruby-2.?.?/wrappers/heapy "$@"
 }
